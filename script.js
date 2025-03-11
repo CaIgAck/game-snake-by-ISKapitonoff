866fbce53f080c2ccc3145481f586145afc6e763
Ivan Kapitonoff <bojenka229@gmail.com>
2025-03-11 12:56:01 +0300

    add refactor and fixes bugs

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-/*
-    1. Научиться в рандомном месте 
-    генерировать голову - готово
-    2. Научить голову ходить вверх - готово
-    3. Научить голову ходить вправо - готово
-    4. Научить голову ходить влево - готово
-    5. Научить голову ходить вниз - готово
-    6. Если голова достигла границ, то game over - ?
-    7. Если голова воткнулась в тело, то game over - ?
-    8. Написать генерацию яблок
-    9. Написать генерацию хвоста
-    10. Написать движение хвоста
-*/
-
 let lastAppleCords = null
 let appleCoords = null
 let snakeHead = null
+let speed = 500
 
 const snakeTail = [];
 const rows = document.querySelectorAll('.row');
 const cols = rows[0].querySelectorAll('.col');
 const baseSpeed = 500
-let speed = 500
 
 const commands = {
     'down': 'down',
@@ -34,14 +20,11 @@ let currentCommand = commands['up'];
 function initGame() {
     if (snakeHead) return
 
-    // Добавляем голову
     generateSnake();
     addSnakeToField();
 
-    //Добавляем яблоко
     initApple();
 
-    // Добавляем контролы
     initControls();
 
     engine()
@@ -55,35 +38,32 @@ function engine() {
             moveSnakeTail()
         }
 
-
         switch (currentCommand) {
             case commands['down']:
-                moveDown()
+                moveByX(true)
                 break;
             case commands['up']:
-                moveUp()
+                moveByX()
                 break;
             case commands['left']:
-                moveLeft()
+                moveByY()
                 break;
             case commands['rigth']:
-                moveRight()
+                moveByY(true)
                 break;
         }
-
-
 
         if (snakeTail.includes(snakeHead)) {
             clearInterval(intervalId)
         }
 
         if (snakeHead === appleCoords) {
-            addNewSnakeTailCell()
 
             lastAppleCords = appleCoords
             removeAppleFromField();
-
             initApple();
+
+            addNewSnakeTailCell()
 
             const speedCoefficient = 0.5
             const snakeTailBaseLength = snakeTail.length < 3 ? 2 : snakeTail.length
@@ -104,17 +84,13 @@ function moveSnakeTail() {
 function removeSnakeTailCell() {
     const lastSnakeTailCell = snakeTail.pop();
 
-    let [coordX, coordY] = lastSnakeTailCell.split('-')
-    const row = document.querySelector(`[data-row='${coordX}']`)
-    row.querySelector(`[data-col='${coordY}']`).classList.remove('snake-tail')
+    mutationEntity({ coords: lastSnakeTailCell, className: 'snake-tail', actionName: 'remove' })
 }
 
 function addSnakeTailCell() {
     snakeTail.unshift(snakeHead)
 
-    const [snakeHeadCoordX, snakeHeadCoordY] = snakeHead.split('-')
-    const newRow = document.querySelector(`[data-row='${snakeHeadCoordX}']`)
-    newRow.querySelector(`[data-col='${snakeHeadCoordY}']`).classList.add('snake-tail')
+    mutationEntity({ coords: snakeHead, className: 'snake-tail', actionName: 'add' })
 }
 
 function addNewSnakeTailCell() {
@@ -136,73 +112,48 @@ function addNewSnakeTailCell() {
             snakeHeadCoordY -= 1
             break;
     }
-    const newSnakeTailCoords = `${snakeHeadCoordX}-${snakeHeadCoordY}`
-
+    const newSnakeTailCoords = coordsFormatted({ x: snakeHeadCoordX, y: snakeHeadCoordY })
     snakeTail.push(newSnakeTailCoords)
-
-    const newRow = document.querySelector(`[data-row='${snakeHeadCoordX}']`)
-    newRow.querySelector(`[data-col='${snakeHeadCoordY}']`).classList.add('snake-tail')
+    mutationEntity({ coords: newSnakeTailCoords, className: 'snake-tail', actionName: 'add' })
 }
 
-function moveUp() {
+function moveByX(isMoveDown = false) {
     let [coordX, coordY] = snakeHead.split('-')
     const row = document.querySelector(`[data-row='${coordX}']`)
     row.querySelector(`[data-col='${coordY}']`).classList.remove('snake')
 
-    coordX -= 1
-    if (coordX >= 0) {
+    if (isMoveDown) {
+        coordX = Number(coordX)
+        coordX += 1
+    } else {
+        coordX -= 1
+    }
+
+    if (coordX < rows.length && coordX >= 0) {
         const newRow = document.querySelector(`[data-row='${coordX}']`)
         newRow.querySelector(`[data-col='${coordY}']`).classList.add('snake')
-        snakeHead = `${coordX}-${coordY}`
+        snakeHead = coordsFormatted({ x: coordX, y: coordY })
     }
-};
+}
 
-function moveDown() {
+function moveByY(isMoveRight = false) {
     let [coordX, coordY] = snakeHead.split('-')
     const row = document.querySelector(`[data-row='${coordX}']`)
-    row.querySelector(`[data-col='${coordY}']`).classList.remove('snake')
-
-    coordX = Number(coordX)
-    coordX += 1
-
-    if (coordX <= rows.length) {
-        const newRow = document.querySelector(`[data-row='${coordX}']`)
-        newRow.querySelector(`[data-col='${coordY}']`).classList.add('snake')
-        snakeHead = `${coordX}-${coordY}`
-    }
-};
-
-function moveLeft() {
-    let [coordX, coordY] = snakeHead.split('-')
-    const row = document.querySelector(`[data-row='${coordX}']`)
-
-    const tempCoordY = coordY
-    coordY -= 1
-
-    if (coordY >= 0) {
-        row.querySelector(`[data-col='${tempCoordY}']`).classList.remove('snake')
-        row.querySelector(`[data-col='${coordY}']`).classList.add('snake')
-        snakeHead = `${coordX}-${coordY}`
-    }
-};
-
-function moveRight() {
-    let [coordX, coordY] = snakeHead.split('-')
-    const row = document.querySelector(`[data-row='${coordX}']`)
-
     const tempCoordY = coordY
 
-    coordY = Number(coordY)
-    coordY += 1
+    if (isMoveRight) {
+        coordY = Number(coordY)
+        coordY += 1
+    } else {
+        coordY -= 1
+    }
 
-    if (coordX <= rows.length) {
+    if (coordY < row.children.length && coordY >= 0) {
         row.querySelector(`[data-col='${tempCoordY}']`).classList.remove('snake')
         row.querySelector(`[data-col='${coordY}']`).classList.add('snake')
-        snakeHead = `${coordX}-${coordY}`
+        snakeHead = coordsFormatted({ x: coordX, y: coordY })
     }
-};
-
-
+}
 
 function initControls() {
     document.addEventListener("keydown", (event) => {
@@ -233,45 +184,54 @@ function initApple() {
 }
 
 function generateApple() {
-    const coordX = Math.floor(Math.random() * 10);
-    const coordY = Math.floor(Math.random() * 10);
-    const appleCoordsFormated = `${coordX}-${coordY}`
+    const [coordX, coordY] = coordsGenerate()
+    const appleCoordsFormated = coordsFormatted({ x: coordX, y: coordY })
 
     if (appleCoordsFormated === snakeHead || snakeTail.includes(appleCoordsFormated)) {
         generateApple()
-    }
-    else {
-        appleCoords = `${coordX}-${coordY}`
+    } else {
+        appleCoords = coordsFormatted({ x: coordX, y: coordY })
     }
 }
 
 function addAppleToField() {
-    const [coordX, coordY] = appleCoords.split('-')
-    const row = document.querySelector(`[data-row='${coordX}']`)
-
-    row.querySelector(`[data-col='${coordY}']`).classList.add('apple')
+    mutationEntity({ coords: appleCoords, className: 'apple', actionName: 'add' })
 }
 
 function removeAppleFromField() {
     if (!lastAppleCords) return
 
-    const [coordX, coordY] = lastAppleCords.split('-')
-    const row = document.querySelector(`[data-row='${coordX}']`)
-
-    row.querySelector(`[data-col='${coordY}']`).classList.remove('apple')
+    mutationEntity({ coords: lastAppleCords, className: 'apple', actionName: 'remove' })
     lastAppleCords = null
 }
 
 function generateSnake() {
-    const coordX = Math.floor(Math.random() * 10);
-    const coordY = Math.floor(Math.random() * 10);
+    const [coordX, coordY] = coordsGenerate()
 
-    snakeHead = `${coordX}-${coordY}`
+    snakeHead = coordsFormatted({ x: coordX, y: coordY })
 }
 
 function addSnakeToField() {
-    const [coordX, coordY] = snakeHead.split('-')
+    mutationEntity({ coords: snakeHead, className: 'snake', actionName: 'add' })
+}
+
+function coordsFormatted({ x, y }) {
+    return `${x}-${y}`
+}
+
+function coordsGenerate() {
+    const coordX = Math.floor(Math.random() * 10);
+    const coordY = Math.floor(Math.random() * 10);
+    return [coordX, coordY]
+}
+
+function mutationEntity({ coords, className, actionName }) {
+    const [coordX, coordY] = coords.split('-')
     const row = document.querySelector(`[data-row='${coordX}']`)
 
-    row.querySelector(`[data-col='${coordY}']`).classList.add('snake')
+    if (actionName === 'add') {
+        row.querySelector(`[data-col='${coordY}']`).classList.add(className)
+    } else {
+        row.querySelector(`[data-col='${coordY}']`).classList.remove(className)
+    }
 }
